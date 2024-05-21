6bccf5ad3fd6a3bc1128008580925a9439d4b3d9
Otávio Alves <oalves@protonmail.com>
2024-05-21 11:55:14 -0400

    Minor changes

--- pomodoro.py
+++ pomodoro.py
@@ -10,7 +10,7 @@ if not os.path.isdir(basedir):
     os.mkdir(basedir)
 
 logfile = f'{basedir}/work.log'
-soundfile = f'{basedir}/notification.mp3'
+soundfile = f'{basedir}/listen.mp3'
 
 stages = ['🮣', '🮦', '🮪', '🮮']
 # stages = ['🬃', '🬅', '🬍', '🬎']
@@ -73,6 +73,25 @@ def float_to_int(num):
     """
     return int(num) + 1 if abs(num - int(num) - 1) < 1e-3 else int(num)
 
+def log_work(project, duration, work_session = None):
+    """
+    Log work to the log file.
+
+    Args:
+        project (str): The project name.
+        duration (int): The duration of the work in seconds.
+    """
+    if work_session is not None:
+        if project not in work_session:
+            work_session[project] = 0
+        work_session[project] += duration
+    with open(logfile, 'a') as f:
+        f.write(f'{time.time() - duration:.0f}\t{project}\t{duration:.0f}\n')
+
+# class MyCustomCompleter(prompt.completion.Completer):
+#     def get_completions(self, document, complete_event):
+#         yield prompt.completion.Completion('completion', start_position=0)
+
 def main():
     """
     Main function to run the Pomodoro timer.
@@ -90,25 +109,49 @@ def main():
         if row.project not in work_session:
             work_session[row.project] = 0
         work_session[row.project] += row.duration
-        
+
     projects = set(log.project)
-    projects_completer = prompt.completion.WordCompleter(projects)
 
     session = prompt.PromptSession()
     while True:
         try:
-            print('    '.join([f'{p} {int_to_ticks(work_session[p]/60/session_duration)}' for p in work_session if work_session[p] > session_duration*60]))
-            project = session.prompt('working on ', completer=projects_completer)
-            projects.add(project)
-            elapsed = timer(session_duration*60)
-            if elapsed > 5:
-                if project not in work_session:
-                    work_session[project] = 0
-                work_session[project] += elapsed
-                with open(logfile, 'a') as f:
-                    f.write(f'{time.time() - elapsed:.0f}\t{project}\t{elapsed:.0f}\n')
-            print('break')
-            timer(break_duration*60)
+            print('    '.join([f'{p} {int_to_ticks(work_session[p]/60/session_duration)}' for p in work_session if work_session[p] > 0.8*session_duration*60]))
+            
+            projects_completer = prompt.completion.NestedCompleter.from_nested_dict({
+                'work': projects,
+                'add': projects,
+                'list': None,
+                'quit': None,
+                })
+            
+            args = session.prompt('pomodoro ~ ', completer=projects_completer).split()
+
+            if not args:
+                continue
+
+            if args[0] == 'quit':
+                break
+
+            if args[0] == 'list':
+                print('\n'.join(projects))
+                continue
+
+            if args[0] == 'add':
+                for a in args[1:]:
+                    projects.add(a)
+                    log_work(a, session_duration*60, work_session=work_session)
+                continue
+
+            if args[0] == 'work' and len(args) > 1:
+                project = args[1]
+                projects.add(project)
+                elapsed = timer(session_duration*60)
+                if elapsed > 10:
+                    log_work(project, elapsed, work_session=work_session)
+                print('break')
+                timer(break_duration*60)
+                continue
+
         except (EOFError, KeyboardInterrupt):
             break
 
